c9e94d9c71512f28bbec64ef38e13f578111dabc
Ankit-298 <ankit29086@gmail.com>
2026-09-16 15:03:06 +0530

    Add installable Citizen PWA

---
+++ citizen/public/citizen-sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'jansetu-citizen-shell-v1';
+const SHELL_URLS = [
+  '/citizen/',
+  '/citizen/index.html',
+  '/citizen/jansetu-logo.png',
+  '/citizen/manifest.webmanifest'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_URLS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const requestUrl = new URL(event.request.url);
+  if (event.request.method !== 'GET' || requestUrl.pathname.startsWith('/api/')) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('/citizen/index.html')))
+  );
+});
